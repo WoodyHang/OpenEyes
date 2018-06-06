e6aaad54b01134d21cf7474eeb344fbfb1df0101
Woodyhang <Woodyhang@QQ.COM>
2018-06-06 09:37:32 +0800

    Initial Commit

---
+++ OpenEyes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenEyes
+//
+//  Created by Woodyhang on 2018/6/6.
+//  Copyright © 2018年 航仔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
